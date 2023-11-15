ab0efb72340da0622411b2fd8cdc6ff0783a3212
Jarrett <124935138+jarrett-vee@users.noreply.github.com>
2023-11-15 07:46:32 -0500

    Update keys.py

--- routes/keys.py
+++ routes/keys.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, flash, request
 from models import User
 from flask_login import login_required, current_user
 
